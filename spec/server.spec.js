8251de6b024fba6c902ee13e6b990387b49a0f53
Pascal <sirpkkarisa001@gmail.com>
2019-11-11 20:00:40 +0300

    Another one

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,19 +1,19 @@
-const app = require('../app');
+// const app = require('../app');
 
-describe('Server', () => {
-  let server;
-  beforeAll(() => {
-    server = require('../server');
-  });
-  afterAll(() => {
-    server.close();
-  });
-  describe('POST/auth/create-user', () => {
-    const data = {};
-    beforeAll(() => {
-      app.post('http://localhost:7000/auth/create-user', (error, res, body) => {
+// describe('Server', () => {
+//   let server;
+//   beforeAll(() => {
+//     server = require('../server');
+//   });
+//   afterAll(() => {
+//     server.close();
+//   });
+//   describe('POST/auth/create-user', () => {
+//     const data = {};
+//     beforeAll(() => {
+//       app.post('http://localhost:7000/auth/create-user', (error, res, body) => {
 
-      });
-    });
-  });
-});
+//       });
+//     });
+//   });
+// });
